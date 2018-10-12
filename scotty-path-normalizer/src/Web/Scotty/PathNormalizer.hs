929c9537960889302be2e04dcfe64851446320e0
Chris Martin <ch.martin@gmail.com>
2018-10-11 22:59:45 -0400

    bring the foldr components to top level

--- scotty-path-normalizer/src/Web/Scotty/PathNormalizer.hs
+++ scotty-path-normalizer/src/Web/Scotty/PathNormalizer.hs
@@ -1,16 +1,33 @@
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Web.Scotty.PathNormalizer where
 
-import Control.Monad (when)
-import Data.ByteString (ByteString)
-import qualified Data.Text as Text
-import qualified Data.Text.Lazy as LText
-import qualified Data.Text.Encoding as Text
-import Data.Text (Text)
+import Control.Monad
+import Data.Bool
+import Data.Either
+import Data.Eq
+import Data.Foldable
+import Data.Function
+import Data.Maybe
+import Data.Ord
 import Network.Wai
 import Numeric.Natural
+import Text.Show
 import Web.Scotty
+
+import Data.ByteString (ByteString)
+import Data.Text       (Text)
+import Prelude         ((+), (-))
+
+import qualified Data.Text          as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy     as LT
+
+slash, dot, up :: Text
+slash = T.pack "/"
+dot   = T.pack "."
+up    = T.pack ".."
 
 anyPath :: RoutePattern
 anyPath = function (const (Just []))
@@ -27,34 +44,34 @@ pathNormalizerAction =
         bs :: ByteString = rawPathInfo req
 
     txt :: Text <-
-        case Text.decodeUtf8' bs of
-            Left _ -> next
+        case T.decodeUtf8' bs of
+            Left _  -> next
             Right x -> return x
 
-    when (txt == Text.pack "/") next
+    when (txt == slash) next
 
     slashRemoved :: Text <-
-        case Text.stripPrefix (Text.pack "/") txt of
+        case T.stripPrefix slash txt of
             Nothing -> next
-            Just x -> return x
+            Just x  -> return x
 
     normalizedSegments <-
-        case (pathNormalize (Text.split (== '/') slashRemoved)) of
-            Invalid -> next
+        case (pathNormalize (T.split (== '/') slashRemoved)) of
+            Invalid       -> next
             AlreadyNormal -> next
             Normalized xs -> return xs
 
     queryText :: Text <-
-        case (Text.decodeUtf8' (rawQueryString req)) of
-            Left _ -> next
+        case (T.decodeUtf8' (rawQueryString req)) of
+            Left _  -> next
             Right x -> return x
 
     let
         redirectUrl =
-            foldMap (\x -> Text.pack "/" <> x) normalizedSegments
-            <> queryText
+            foldMap (\x -> slash `T.append` x) normalizedSegments
+            `T.append` queryText
 
-    (redirect . LText.fromStrict) redirectUrl
+    redirect (LT.fromStrict redirectUrl)
 
 data Result = Invalid | AlreadyNormal | Normalized [Text]
     deriving Show
@@ -62,22 +79,22 @@ data Result = Invalid | AlreadyNormal | Normalized [Text]
 -- |
 -- A path that's already in normal form:
 --
--- >>> pathNormalize [Text.pack "one", Text.pack "two", Text.pack "three"]
+-- >>> pathNormalize [T.pack "one", T.pack "two", T.pack "three"]
 -- AlreadyNormal
 --
 -- A path that contains empty segments:
 --
--- >>> pathNormalize [Text.pack "", Text.pack "one", Text.pack ".", Text.pack "two", Text.pack "three"]
+-- >>> pathNormalize [T.pack "", T.pack "one", T.pack ".", T.pack "two", T.pack "three"]
 -- Normalized ["one","two","three"]
 --
 -- A path that goes "up" a directory:
 --
--- >>> pathNormalize [Text.pack "one", Text.pack "two", Text.pack "three", Text.pack "..", Text.pack "four"]
+-- >>> pathNormalize [T.pack "one", T.pack "two", T.pack "three", T.pack "..", T.pack "four"]
 -- Normalized ["one","two","four"]
 --
 -- A path that goes up too far:
 --
--- >>> pathNormalize [Text.pack "one", Text.pack "..", Text.pack "..", Text.pack "two", Text.pack "three"]
+-- >>> pathNormalize [T.pack "one", T.pack "..", T.pack "..", T.pack "two", T.pack "three"]
 -- Invalid
 --
 -- The empty string is a normalized path:
@@ -88,15 +105,20 @@ data Result = Invalid | AlreadyNormal | Normalized [Text]
 pathNormalize :: [Text] -> Result
 pathNormalize segments =
 
-    case (foldr f ([], 0 :: Natural, False) segments) of
-        (_, _, False) -> AlreadyNormal
-        (xs, 0, _)    -> Normalized xs
-        _             -> Invalid
+    case (foldr step init segments) of
+        State _  _ False -> AlreadyNormal
+        State xs 0 _     -> Normalized xs
+        _                -> Invalid
 
-  where
-    f x (xs, parents, different)
-        | x == Text.pack ""   = (    xs, parents,     True)
-        | x == Text.pack "."  = (    xs, parents,     True)
-        | x == Text.pack ".." = (    xs, parents + 1, True)
-        | parents > 0         = (    xs, parents - 1, True)
-        | otherwise           = (x : xs, parents,     different)
+data State = State [Text] Natural Bool
+
+init :: State
+init = State [] 0 False
+
+step :: Text -> State -> State
+step x (State xs parents different)
+    | x == T.empty  =  State       xs    parents       True
+    | x == dot      =  State       xs    parents       True
+    | x == up       =  State       xs   (parents + 1)  True
+    | parents > 0   =  State       xs   (parents - 1)  True
+    | otherwise     =  State  (x : xs)   parents       different
